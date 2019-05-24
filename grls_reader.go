c3424bf4c16bd6dff897c5703be8c107506a8ff0
rummolprod999 <rummolprod999@gmail.com>
2019-05-24 15:56:36 +0300

    update

--- grls_reader.go
+++ grls_reader.go
@@ -95,7 +95,7 @@ func (t *GrlsReader) extractXlsData(nameFile string) {
 }
 
 func (t *GrlsReader) insertToBase(sheet *xls.WorkSheet) {
-	db, err := sql.Open("sqlite3", "grls.db")
+	db, err := sql.Open("sqlite3", "file:grls.db?_journal_mode=OFF")
 	if err != nil {
 		Logging(err)
 		return
